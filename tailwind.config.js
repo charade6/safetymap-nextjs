67e27148a0c3183d438b53c7e7911b8f9b2fbf3d
charade6 <charade6@naver.com>
2022-10-08 03:13:26 +0900

    chore: 세팅

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: [
+    './layout/Layout.tsx',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      boxShadow: {
+        nav: '0 4px 4px rgba(0, 0, 0, 0.25)',
+      },
+      backgroundImage: {
+        running: "url('/ico/icon-running.svg')",
+        map: "url('/ico/icon-location.svg')",
+        money: "url('/ico/icon-money.svg')",
+      },
+    },
+  },
+  plugins: [],
+};
