dad8cc83f1830e29f6fd6ef8df7fb0203330ba53
charade6 <charade6@naver.com>
2022-10-10 11:37:01 +0900

    feat: 공공데이터 api fetch

--- components/NaverMap.tsx
+++ components/NaverMap.tsx
@@ -1,8 +1,52 @@
+import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
+import {
+  TemporaryHousingFacility,
+  EarthquakeIndoors,
+  EarthquakeOutdoorsShelter,
+  TsunamiShelter,
+} from '../types/apiType';
 
 export default function NaverMap() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [naverMap, setNaverMap] = useState<naver.maps.Map>();
+  const [api, setApi] = useState<
+    | TemporaryHousingFacility[]
+    | EarthquakeIndoors[]
+    | EarthquakeOutdoorsShelter[]
+    | TsunamiShelter[]
+  >();
+
+  const getData = async (id: number) => {
+    const menuList: {
+      id: number;
+      name: string;
+      page: number;
+    }[] = [
+      { id: 1, name: 'TemporaryHousing', page: 15 },
+      { id: 2, name: 'EarthquakeIndoors', page: 6 },
+      { id: 3, name: 'EarthquakeOutdoors', page: 11 },
+      { id: 4, name: 'TsunamiShelter', page: 1 },
+    ];
+    const menu = menuList.find((e) => e.id === id);
+
+    const fetchList = [];
+    for (let i = 0; i < menu!.page; i++) {
+      fetchList.push(axios.get(`/api/${menu!.name}/${i + 1}`));
+    }
+
+    await axios.all(fetchList).then(
+      axios.spread((...responses) => {
+        const list:
+          | TemporaryHousingFacility[]
+          | EarthquakeIndoors[]
+          | EarthquakeOutdoorsShelter[]
+          | TsunamiShelter[] = [];
+        responses.forEach((e) => list.push(...e.data));
+        setApi(list);
+      }),
+    );
+  };
 
   const initMap = () => {
     if (mapRef.current) {
@@ -17,11 +61,14 @@ export default function NaverMap() {
 
   useEffect(() => {
     initMap();
-  }, []);
+    getData(1);
+  }, [mapRef]);
 
   useEffect(() => {
-    console.log(naverMap);
-  }, [naverMap]);
+    if (api) {
+      console.log(api);
+    }
+  }, [api]);
 
   return <div className="w-full h-screen" ref={mapRef} />;
 }
