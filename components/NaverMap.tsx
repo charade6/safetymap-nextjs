ca83117451134a225f238a12468b5f6f2ce05dfa
charade6 <charade6@naver.com>
2022-10-11 12:39:47 +0900

    feat: addMarker

--- components/NaverMap.tsx
+++ components/NaverMap.tsx
@@ -1,19 +1,27 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import axios from 'axios';
 import { useEffect, useRef, useState } from 'react';
 import {
-  TemporaryHousingFacility,
+  TemporaryHousing,
   EarthquakeIndoors,
-  EarthquakeOutdoorsShelter,
+  EarthquakeOutdoors,
   TsunamiShelter,
 } from '../types/apiType';
 
 export default function NaverMap() {
   const mapRef = useRef<HTMLDivElement>(null);
   const [naverMap, setNaverMap] = useState<naver.maps.Map>();
+  const markerList = useRef<
+    {
+      marker: naver.maps.Marker;
+      infowindow: naver.maps.InfoWindow;
+    }[]
+  >([]);
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [api, setApi] = useState<
-    | TemporaryHousingFacility[]
+    | TsunamiShelter[]
     | EarthquakeIndoors[]
-    | EarthquakeOutdoorsShelter[]
+    | TemporaryHousing[]
     | TsunamiShelter[]
   >();
 
@@ -38,10 +46,11 @@ export default function NaverMap() {
     await axios.all(fetchList).then(
       axios.spread((...responses) => {
         const list:
-          | TemporaryHousingFacility[]
+          | TemporaryHousing[]
           | EarthquakeIndoors[]
-          | EarthquakeOutdoorsShelter[]
+          | EarthquakeOutdoors[]
           | TsunamiShelter[] = [];
+
         responses.forEach((e) => list.push(...e.data));
         setApi(list);
       }),
@@ -59,16 +68,78 @@ export default function NaverMap() {
     }
   };
 
+  const updateMarkers = (map: naver.maps.Map) => {
+    const mapBounds = map.getBounds();
+    const max = mapBounds.getMax();
+    const min = mapBounds.getMin();
+    const filt = api!.filter(
+      (e) =>
+        e.xcord > min.x &&
+        e.xcord < max.x &&
+        e.ycord > min.y &&
+        e.ycord < max.y,
+    );
+    for (let i = 0; i < markerList.current.length; i++) {
+      if (!mapBounds.hasPoint(markerList.current[i].marker.getPosition())) {
+        markerList.current[i].marker.setMap(null);
+      }
+    }
+    markerList.current = markerList.current.filter(
+      (e) => e.marker.getMap() !== null,
+    );
+    for (let i = 0; i < filt.length; i++) {
+      const marker = new naver.maps.Marker({
+        position: new naver.maps.LatLng(filt[i].ycord, filt[i].xcord),
+        map,
+      });
+      const infowindow = new naver.maps.InfoWindow({
+        content: `<div style="width:150px;text-align:center;padding:10px;">${filt[i].dtl_adres}</div>`,
+      });
+      naver.maps.Event.addListener(marker, 'click', () => {
+        if (infowindow.getMap()) {
+          infowindow.close();
+        } else {
+          infowindow.open(map, marker);
+        }
+      });
+      naver.maps.Event.addListener(naverMap, 'drag', () => {
+        infowindow.close();
+      });
+      markerList.current.push({ marker, infowindow });
+    }
+  };
+
   useEffect(() => {
     initMap();
-    getData(1);
+    getData(2);
   }, [mapRef]);
 
   useEffect(() => {
-    if (api) {
-      console.log(api);
+    if (api && naverMap) {
+      updateMarkers(naverMap);
+      naverMap.addListener('idle', () => {
+        if (naverMap.getZoom() > 13) {
+          updateMarkers(naverMap);
+        }
+      });
+      naverMap.addListener('zoom_changed', () => {
+        if (naverMap.getZoom() < 14) {
+          for (let i = 0; i < markerList.current.length; i++) {
+            markerList.current[i].infowindow.close();
+            markerList.current[i].marker.setMap(null);
+          }
+          markerList.current = [];
+        }
+      });
     }
-  }, [api]);
+  }, [api, naverMap]);
 
-  return <div className="w-full h-screen" ref={mapRef} />;
+  return (
+    <div>
+      {!api && (
+        <div className="absolute z-30 w-full h-screen bg-black opacity-30" />
+      )}
+      <div className="w-full h-screen" ref={mapRef} />
+    </div>
+  );
 }
