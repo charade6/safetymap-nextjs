1da6503f3ae45b7c66a3140f23aa6db7138b855d
charade6 <charade6@naver.com>
2022-10-11 13:59:28 +0900

    design: footer 변경

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { PropsWithChildren } from 'react';
 import { useRouter } from 'next/router';
 import NavItem from '../components/NavItem';
+import IcoLogo from '../public/ico/icon-logo.svg';
 
 export default function Layout({ children }: PropsWithChildren) {
   const router = useRouter();
@@ -20,7 +21,19 @@ export default function Layout({ children }: PropsWithChildren) {
         </ul>
       </nav>
       {children}
-      <footer />
+      {router.pathname === '/map' || router.pathname === '/404' ? null : (
+        <footer className="flex flex-col h-[200px] bg-[#F2F2F2] text-center text-[#A2A8A5] justify-center">
+          <h3 className="mb-2 text-xl font-bold">
+            <IcoLogo className="inline mr-3" />
+            주변 대피소 찾기
+          </h3>
+          <p>
+            <span className="font-bold">학과</span> 컴퓨터정보학부 &#124;&#160;
+            <span className="font-bold">제작</span> 학점대피소
+          </p>
+          <p>Copyright &#169; credit shelter All Rights Reserved</p>
+        </footer>
+      )}
     </div>
   );
 }
