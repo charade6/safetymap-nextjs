23d47fa6c8bac9d0fa8624966551845933556955
charade6 <charade6@naver.com>
2022-11-02 10:26:24 +0900

    fix: next/image로 변경

--- components/common/Loading.tsx
+++ components/common/Loading.tsx
@@ -1,15 +1,14 @@
-/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image';
+import loading from '../../assets/loading.gif';
 
 const Loading = () => {
   return (
-    <>
-      <div className="fixed top-0 left-0 z-30 w-full h-screen bg-[#999] opacity-50" />
-      <img
-        className="absolute z-50 w-10 translate-x-[-50%] translate-y-[-50%] top-2/4 left-2/4"
-        src="/loading.gif"
-        alt="img"
-      />
-    </>
+    <div className="absolute top-0 left-0 z-30 w-full h-screen">
+      <div className="h-full bg-[#999] opacity-50" />
+      <div className="absolute translate-x-[-50%] translate-y-[-50%] top-2/4 left-2/4">
+        <Image src={loading} width={40} height={40} layout="fixed" priority />
+      </div>
+    </div>
   );
 };
 
