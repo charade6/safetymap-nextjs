ef33317d3ab7e0d13aeb02e37ce68d096e1ebe03
charade6 <charade6@naver.com>
2022-11-09 06:44:00 +0900

    fix: 링크오타수정

--- util/kakaoShare.ts
+++ util/kakaoShare.ts
@@ -26,8 +26,8 @@ export const kakaoShare = ({
       imageUrl:
         'https://64.media.tumblr.com/db707ec4d468a23273fe4353c86fbd3e/df33f1c96f9eea5b-d6/s2048x3072/d0b88139d9f9250ccfff428c3ebc5d4a315025bf.jpg',
       link: {
-        mobileWebUrl: `https://safetymap-nextjs.vercel.app/map?type=${maptype}&lat=${lat}&${lng}`,
-        webUrl: `https://safetymap-nextjs.vercel.app/map?type=${maptype}&lat=${lat}&${lng}`,
+        mobileWebUrl: `https://safetymap-nextjs.vercel.app/map?type=${maptype}&lat=${lat}&lng=${lng}`,
+        webUrl: `https://safetymap-nextjs.vercel.app/map?type=${maptype}&lat=${lat}&lng=${lng}`,
       },
     },
     buttonTitle: '홈페이지로',
